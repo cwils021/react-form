b976a8d5d3cc19fb9e88ae5553fa0d98d616ec4a
Christopher Wilson <chrisgwilson678@gmail.com>
2021-06-26 23:54:01 -0400

    setup state and Context Provider

--- src/Consts.js
+++ src/Consts.js
@@ -12,55 +12,77 @@ export const industries = ["Select an Industry","Accounting", "Software developm
 
 export const selectionStrings = ['Select...', 'Yes', 'No'];
 
-export const  Schema = {
+export const Schema = {
 	"$schema": "http://json-schema.org/draft-07/schema#",
-	"$id" : "#/jobPost.Schema.json",
+	"$id": "#/jobPost.Schema.json",
 	"title": "FormValidationSchema",
 	"type": "object",
 	"properties": {
-	  "company_name": {
-		"title" : "company_name",
-		"type": "string",
-		"minLength": 1 ,
-		"maxLength": 2000
-	  },
-	  "role": {
-		"title": "role",
-	   	 "enum": ["Customer Success", "Fullstack Engineer", "Product Manager"]
-	  },
-	  "industry": {
-		"title": "industry",
-	    	"enum": ["Accounting", "Software development", "Landscaping"]
-	  }
+		"company_name": {
+			"title": "company_name",
+			"type": "string",
+			"minLength": 1,
+			"maxLength": 2000
+		},
+		"role": {
+			"title": "role",
+			"enum": ["Customer Success", "Fullstack Engineer", "Product Manager"]
+		},
+		"industry": {
+			"title": "industry",
+			"enum": ["Accounting", "Software development", "Landscaping"]
+		}
 	},
 	"allOf": [
 		{
 			"if": {
-				"properties": {"role":{"const": "Fullstack Engineer"}}
+				"properties": { "role": { "const": "Fullstack Engineer" } }
 			},
 			"then": {
-				"properties": {"technical_requirement":{"enum":['Yes','No'],
-												"title":"technical_requirement"}},
+				"properties": {
+					"technical_requirement": {
+						"enum": ['Yes', 'No'],
+						"title": "technical_requirement"
+					}
+				},
 				"required": ["technical_requirement"]
 			}
 		},
 		{
-		"if": {
-			"properties": {
-				"role":{"const": "Product Manager"},
-				"industry" : {"const" : "Software development"}
-			}
-		},
-		"then":	{
-				"properties": {"technical_requirement":{"enum":['Yes','No'],
-												"title":"technical_requirement"}},
+			"if": {
+				"properties": {
+					"role": { "const": "Product Manager" },
+					"industry": { "const": "Software development" }
+				}
+			},
+			"then": {
+				"properties": {
+					"technical_requirement": {
+						"enum": ['Yes', 'No'],
+						"title": "technical_requirement"
+					}
+				},
 				"required": ["technical_requirement"]
-		}
+			}
 		}
 	],
 	"required": [
-	  "company_name",
-	  "role",
-	  "industry"
+		"company_name",
+		"role",
+		"industry"
 	]
-}
+};
+
+export const validTestPost = {
+	company_name: "ABC Company Inc.",
+	role: "Fullstack Engineer",
+	industry: "Software development",
+	technical_requirement: "Yes"
+};
+
+export const invalidTestPost = {
+	company_name: "ABC Company Inc.",
+	role: "Fullstack Engineer",
+	industry: "Software development",
+
+};
--- src/providers/FormContextProvider.js
+++ src/providers/FormContextProvider.js
@@ -1,2 +1,25 @@
 // todo set state
 // todo create provider
+import React, { useState } from 'react';
+
+const FormContext = React.createContext([
+	{},
+	() => { }
+]);
+
+const FormContextProvider = (props) => {
+	const [state, setState] = useState({
+		show: false,
+		isSubmitting: false,
+		values: {},
+		errors: {},
+	});
+
+	return (
+		<FormContext.Provider value={[state, setState]}>
+			{props.children}
+		</FormContext.Provider>
+	);
+}
+
+export { FormContext, FormContextProvider }
