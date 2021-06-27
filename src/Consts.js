9ebd736b76edf22613e984107dc5662dba7d0e20
Christopher Wilson <chrisgwilson678@gmail.com>
2021-06-26 20:25:31 -0400

    add consts

--- src/Consts.js
+++ src/Consts.js
@@ -1,2 +1,66 @@
-// todo add enum consts
-// todo copy in Schema and rm Schema.js
+/*********************************************************************************
+*                              Author: Chris Wilson                              *
+*                          Source Language: javascript                           *
+*                                                                                *
+*                            --- Code Description ---                            *
+*                            Constants for react-form                            *
+*********************************************************************************/
+
+export const roles = ["Select a Role","Customer Success", "Fullstack Engineer", "Product Manager"];
+
+export const industries = ["Select an Industry","Accounting", "Software development", "Landscaping"];
+
+export const selectionStrings = ['Select...', 'Yes', 'No'];
+
+export const  Schema = {
+	"$schema": "http://json-schema.org/draft-07/schema#",
+	"$id" : "#/jobPost.Schema.json",
+	"title": "FormValidationSchema",
+	"type": "object",
+	"properties": {
+	  "company_name": {
+		"title" : "company_name",
+		"type": "string",
+		"minLength": 1 ,
+		"maxLength": 2000
+	  },
+	  "role": {
+		"title": "role",
+	   	 "enum": ["Customer Success", "Fullstack Engineer", "Product Manager"]
+	  },
+	  "industry": {
+		"title": "industry",
+	    	"enum": ["Accounting", "Software development", "Landscaping"]
+	  }
+	},
+	"allOf": [
+		{
+			"if": {
+				"properties": {"role":{"const": "Fullstack Engineer"}}
+			},
+			"then": {
+				"properties": {"technical_requirement":{"enum":['Yes','No'],
+												"title":"technical_requirement"}},
+				"required": ["technical_requirement"]
+			}
+		},
+		{
+		"if": {
+			"properties": {
+				"role":{"const": "Product Manager"},
+				"industry" : {"const" : "Software development"}
+			}
+		},
+		"then":	{
+				"properties": {"technical_requirement":{"enum":['Yes','No'],
+												"title":"technical_requirement"}},
+				"required": ["technical_requirement"]
+		}
+		}
+	],
+	"required": [
+	  "company_name",
+	  "role",
+	  "industry"
+	]
+}
